aff81e16c2811637a8ab3c59e89670474720666b
Mckoj <vestermckoj@gmail.com>
2026-03-19 09:45:16 +0000

    feat:initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
+import { Toaster } from "@/components/ui/sonner"
+import BetterAuthUIProvider from "@/providers/better-auth-ui-provider"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +26,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+     <BetterAuthUIProvider>{children}</BetterAuthUIProvider>
+        <Toaster />
+      </body>
     </html>
+    
   );
 }
