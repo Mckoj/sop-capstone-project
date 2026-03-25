6cc2ab3fd5ac42addd7418ee1e3e947717bd49e5
Gerich <brefogerich@gmail.com>
2026-03-24 16:02:34 -1200

    Reciept feature

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,9 +2,16 @@ import "dotenv/config";
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from './generated/prisma/client'
 
-const connectionString = `${process.env.DATABASE_URL}`
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
-const adapter = new PrismaPg({ connectionString })
-const prisma = new PrismaClient({ adapter })
+function createPrismaClient() {
+  const connectionString = `${process.env.DATABASE_URL}`
+  const adapter = new PrismaPg({ connectionString })
+  return new PrismaClient({ adapter })
+}
+
+const prisma = globalForPrisma.prisma || createPrismaClient()
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
 export { prisma }
--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  allowedDevOrigins: ["100.66.100.55", "192.168.133.79"],
 };
 
 export default nextConfig;
